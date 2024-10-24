e1a77cf66f510b39e8c6b064b0f4828f220c52a4
snehapatidar5 <sneha.mongooseintern@gmail.com>
2024-10-24 15:37:13 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todo'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
